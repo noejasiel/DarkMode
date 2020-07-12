44bd0a15ef8e11d9e1b63dfb489a764b29c885a1
noejasiel <55604384+noejasiel@users.noreply.github.com>
2020-07-12 00:52:10 -0500

    add casi mod obscuro

--- main.js
+++ main.js
@@ -3,4 +3,23 @@ console.log(btn);
 
 btn.addEventListener("click", function () {
   btn.classList.toggle("active");
+  var padre = btn.parentElement;
+  Imprimir(padre);
 });
+
+function Imprimir(padre) {
+  console.log(padre.parentElement.parentElement, "jbhknklm");
+  var padreMenor = padre.parentElement;
+  padreMenor.classList.toggle("content-dark");
+  padre = padre.parentElement.parentElement;
+  padre.classList.toggle("conteiner-total-dark");
+  var result = document
+    .getElementsByClassName("result")[0]
+    .classList.toggle("result-dark");
+  console.log(result[0]);
+  var prueba = document.querySelectorAll("button");
+  console.log(prueba.length);
+  for (let i = 1; i < prueba.length - 6; i++) {
+    console.log(prueba[i].classList.toggle("btn-dark"));
+  }
+}
